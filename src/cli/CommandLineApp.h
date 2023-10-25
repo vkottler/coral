b578558d8a923bbdb6f5c6f0a322a769820f303f
Vaughn Kottler <vaughnkottler@gmail.com>
2023-10-24 22:59:53 -0500

    Finish basic CLi app implementation

--- src/cli/CommandLineApp.h
+++ src/cli/CommandLineApp.h
@@ -13,7 +13,10 @@ namespace Coral
 
 static constexpr std::size_t default_max_line = 256;
 
-template <std::size_t depth = default_max_line,
+static constexpr std::size_t default_max_commands = 16;
+
+template <std::size_t max_commands = default_max_commands,
+          std::size_t depth = default_max_line,
           std::size_t max_args = default_max_args, typename element_t = char,
           class T = PrintfLogger>
 class ElementCommandLineApp : public HasLogInterface<T>
@@ -24,7 +27,10 @@ class ElementCommandLineApp : public HasLogInterface<T>
 
     using Handler = std::function<void(CommandLine &)>;
 
-    ElementCommandLineApp(typename Processor::Buffer &input,
+    using CommandRegistration = std::function<void(ElementCommandLineApp &)>;
+
+    ElementCommandLineApp(CommandRegistration register_commands,
+                          typename Processor::Buffer &input,
                           LogInterface<T> *_log = nullptr)
         : HasLogInterface<T>(_log),
           processor(
@@ -32,30 +38,48 @@ class ElementCommandLineApp : public HasLogInterface<T>
               [this](const element_t **argv, std::size_t argc) {
                   process(argv, argc);
               },
-              true /* auto_poll */)
+              true /* auto_poll */),
+          commands(), command_index(0)
     {
-        this->logger = _log;
         add_handler("help", [this](CommandLine &cli) { help(cli); });
+        register_commands(*this);
     }
 
     /* interface for registering a command */
     void add_handler(const element_t *command, Handler handler,
+                     const char *help = nullptr,
                      LogInterface<T> *_log = nullptr)
     {
         this->log(_log, "Added command handler for '%s'.\n", command);
 
-        /* need to figure out a data structure for these */
-        (void)handler;
+        commands[command_index].command = command;
+        commands[command_index].handler = handler;
+        commands[command_index].help = help;
+        command_index++;
     }
 
     void process(const element_t **argv, std::size_t argc)
     {
         CommandLine cli(argv, argc, this->logger);
 
-        this->log(nullptr, "process\n");
+        bool handled = false;
 
-        /* check for a command handler, otherwise do help */
-        help(cli);
+        for (std::size_t i = 0; i < command_index; i++)
+        {
+            auto &cmd = commands[i];
+            handled = cmd.handle(cli);
+            if (handled)
+            {
+                break;
+            }
+        }
+
+        if (not handled)
+        {
+            this->log(nullptr, "Command '%s' not found.\n",
+                      cli.get_command().c_str());
+            help(cli);
+        }
     }
 
   protected:
@@ -63,11 +87,40 @@ class ElementCommandLineApp : public HasLogInterface<T>
 
     void help(CommandLine &cli)
     {
-        /* dump info about registered commands */
         (void)cli;
 
-        this->log(nullptr, "help command\n");
+        this->log(nullptr, "%zu commands registered:\n", command_index);
+
+        for (std::size_t i = 0; i < command_index; i++)
+        {
+            auto &cmd = commands[i];
+            this->log(nullptr, "  %s: %s\n", cmd.command,
+                      (cmd.help) ? cmd.help : "(unknown)");
+        }
     }
+
+    struct Command
+    {
+        const element_t *command;
+        Handler handler;
+        const char *help = nullptr;
+
+        bool handle(CommandLine &cli)
+        {
+            bool should_handle =
+                strcmp(command, cli.get_command().c_str()) == 0;
+
+            if (should_handle)
+            {
+                handler(cli);
+            }
+
+            return should_handle;
+        }
+    };
+
+    std::array<Command, max_commands> commands;
+    std::size_t command_index;
 };
 
 using CommandLineApp = ElementCommandLineApp<>;
